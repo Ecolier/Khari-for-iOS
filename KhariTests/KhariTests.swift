5b90052d00fc3d251427169ca97040188f0c4869
Evan Gruère <evan.gruere@gmail.com>
2019-12-05 12:48:21 +0100

    Initial Commit

---
+++ KhariTests/KhariTests.swift
@@ -0,0 +1,34 @@
+//
+//  KhariTests.swift
+//  KhariTests
+//
+//  Created by Evan Gruère on 05/12/2019.
+//  Copyright © 2019 Evan Gruère. All rights reserved.
+//
+
+import XCTest
+@testable import Khari
+
+class KhariTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
